75163538ed4e5c9d31a6d4514fd049e9f53ff568
Abhay-Chand <chandabhay639@gmail.com>
2026-09-09 19:37:55 +0530

    Add patient data management endpoints and JSON data file

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Path, HTTPException
+import json
 app = FastAPI()
+
+def load_data():
+    with open('patients.json', 'r') as f:
+        data = json.load(f)
+    return data
 
 @app.get("/") # / means home endpoint
 # uvicorn main:app  --> we can access the page but if we do any chances then we have to restart the server 
@@ -17,4 +22,19 @@ def greet():
 def greet_name(name:str, age:int):
     return {"Message":f"hello {name} and i am {age} year old"}
 
+@app.get('/about')
+def about():
+    return {'message':'A fully fucntional appi to manage your project records'}
+@app.get('/view')
+def view():
+    data = load_data()
 
+    return data
+@app.get('/patient/{patient_id}')
+def view_patient(patient_id: str = Path(...,description='ID of the patient in the DB',example='P002')):
+    # load all the patient
+    data = load_data()
+
+    if patient_id in data:
+        return data[patient_id]
+    raise HTTPException(status_code=404, detail='Patient not found')
